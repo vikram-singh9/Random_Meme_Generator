8b9da18cb6382a14e6c99a7a31609028a3881fb9
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-15 19:54:19 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
 import streamlit as st
 import requests
 
+st.title("😁 Random Meme generator 😂")
+st.subheader("It's simple meme generator for fun fetches meme from the real time API.")
+
+if st.button("Generate meme"):
+
+    try:
+        response = requests.get("https://meme-api.com/gimme")
+        if response.status_code == 200:
+            data = response.json()
+            result = data["url"]
+            a = data["author"]
+            st.image(result, caption="funny😂", width=500)
+            st.write(a)
+        elif response.status_code != 200:
+            st.error("error fetching memes")
+            
+    
+    except:
+        st.write("api not fetcing data from the backend")
+    
